0c19de55cf13da6d74716a5e26cd7a342ae251f6
TerrellRoulston <terrellroulston@hotmail.ca>
2024-12-18 09:02:32 -0400

    sample bg for all vac spp.

--- r_scripts/vacc_bg.R
+++ r_scripts/vacc_bg.R
@@ -35,6 +35,30 @@ wclim <- geodata::worldclim_global(var = 'bio', res = 2.5, version = '2.1', path
 # Load occurrence data ----------------------------------------------------
 occ_angThin <- readRDS(file = './occ_data/thinned/occ_angThin.Rdata')
 occ_arbThin <- readRDS(file = './occ_data/thinned/occ_arbThin.Rdata')
+occ_borThin <- readRDS(file = './occ_data/thinned/occ_borThin.Rdata')
+occ_cesThin <- readRDS(file = './occ_data/thinned/occ_cesThin.Rdata')
+occ_corThin <- readRDS(file = './occ_data/thinned/occ_corThin.Rdata')
+occ_craThin <- readRDS(file = './occ_data/thinned/occ_craThin.Rdata')
+occ_darThin <- readRDS(file = './occ_data/thinned/occ_darThin.Rdata')
+occ_delThin <- readRDS(file = './occ_data/thinned/occ_delThin.Rdata')
+occ_eryThin <- readRDS(file = './occ_data/thinned/occ_eryThin.Rdata')
+occ_hirThin <- readRDS(file = './occ_data/thinned/occ_hirThin.Rdata')
+occ_macThin <- readRDS(file = './occ_data/thinned/occ_macThin.Rdata')
+occ_memThin <- readRDS(file = './occ_data/thinned/occ_memThin.Rdata')
+occ_mysThin <- readRDS(file = './occ_data/thinned/occ_mysThin.Rdata')
+occ_myrThin <- readRDS(file = './occ_data/thinned/occ_myrThin.Rdata')
+occ_mtuThin <- readRDS(file = './occ_data/thinned/occ_mtuThin.Rdata')
+occ_ovaThin <- readRDS(file = './occ_data/thinned/occ_ovaThin.Rdata')
+occ_ovtThin <- readRDS(file = './occ_data/thinned/occ_ovtThin.Rdata')
+occ_oxyThin <- readRDS(file = './occ_data/thinned/occ_oxyThin.Rdata')
+occ_palThin <- readRDS(file = './occ_data/thinned/occ_palThin.Rdata')
+occ_parThin <- readRDS(file = './occ_data/thinned/occ_parThin.Rdata')
+occ_scoThin <- readRDS(file = './occ_data/thinned/occ_scoThin.Rdata')
+occ_staThin <- readRDS(file = './occ_data/thinned/occ_staThin.Rdata')
+occ_tenThin <- readRDS(file = './occ_data/thinned/occ_tenThin.Rdata')
+occ_uliThin <- readRDS(file = './occ_data/thinned/occ_uliThin.Rdata')
+occ_virThin <- readRDS(file = './occ_data/thinned/occ_virThin.Rdata')
+occ_vidThin <- readRDS(file = './occ_data/thinned/occ_vidThin.Rdata')
 
 # Extract ecogregions for each spp. ---------------------------------------
 # V. angustifolium 
@@ -181,7 +205,6 @@ ecoNA_ova <- terra::subset(ecoNA, ecoNA$NA_L2CODE %in% eco_ova_code) # subset ec
 elasped <- (Sys.time() - tm) %>% print()
 saveRDS(ecoNA_ova, file = './bg_data/eco_regions/ecoNA_ova.Rdata') # save Ecoregions for downstream
 
-#STOPPED HERE
 # V. ovatum 
 tm <- Sys.time()
 eco_ovt <- extract(ecoNA, occ_ovtThin)
@@ -245,6 +268,15 @@ ecoNA_ten <- terra::subset(ecoNA, ecoNA$NA_L2CODE %in% eco_ten_code) # subset ec
 elasped <- (Sys.time() - tm) %>% print()
 saveRDS(ecoNA_ten, file = './bg_data/eco_regions/ecoNA_ten.Rdata') # save Ecoregions for downstream
 
+# V. uliginosum
+tm <- Sys.time()
+eco_uli <- extract(ecoNA, occ_uliThin)
+eco_uli_code <- eco_uli$NA_L2CODE %>% unique() 
+eco_uli_code <- eco_uli_code[!is.na(eco_uli_code) & eco_uli_code != '0.0']  #remove the 'water' '0.0' and NA ecoregions
+ecoNA_uli <- terra::subset(ecoNA, ecoNA$NA_L2CODE %in% eco_uli_code) # subset eco region spat vector by the codes
+elasped <- (Sys.time() - tm) %>% print()
+saveRDS(ecoNA_uli, file = './bg_data/eco_regions/ecoNA_uli.Rdata') # save Ecoregions for downstream
+
 # V. virgatum 
 tm <- Sys.time()
 eco_vir <- extract(ecoNA, occ_virThin)
@@ -265,6 +297,7 @@ saveRDS(ecoNA_vid, file = './bg_data/eco_regions/ecoNA_vid.Rdata') # save Ecoreg
 
 
 # Load Ecoregions ---------------------------------------------------------
+ecoNA_arb <- readRDS(file = "./bg_data/eco_regions/ecoNA_arb.Rdata")
 ecoNA_ang <- readRDS(file = "./bg_data/eco_regions/ecoNA_ang.Rdata")
 ecoNA_arb <- readRDS(file = "./bg_data/eco_regions/ecoNA_arb.Rdata")
 ecoNA_bor <- readRDS(file = "./bg_data/eco_regions/ecoNA_bor.Rdata")
@@ -278,25 +311,129 @@ ecoNA_hir <- readRDS(file = "./bg_data/eco_regions/ecoNA_hir.Rdata")
 ecoNA_mac <- readRDS(file = "./bg_data/eco_regions/ecoNA_mac.Rdata")
 ecoNA_mem <- readRDS(file = "./bg_data/eco_regions/ecoNA_mem.Rdata")
 ecoNA_mys <- readRDS(file = "./bg_data/eco_regions/ecoNA_mys.Rdata")
-ecoNA_mtu <- readRDS(file = "./bg_data/eco_regions/ecoNA_mtu.Rdata")
+ecoNA_myr <- readRDS(file = "./bg_data/eco_regions/ecoNA_myr.Rdata")
+ecoNA_mtu <- readRDS(file = "./bg_data/eco_regions/ecoNA_mtu.Rdata") 
+ecoNA_ova <- readRDS(file = "./bg_data/eco_regions/ecoNA_ova.Rdata") 
+ecoNA_ovt <- readRDS(file = "./bg_data/eco_regions/ecoNA_ovt.Rdata")
+ecoNA_oxy <- readRDS(file = "./bg_data/eco_regions/ecoNA_oxy.Rdata")
+ecoNA_pal <- readRDS(file = "./bg_data/eco_regions/ecoNA_pal.Rdata")
+ecoNA_par <- readRDS(file = "./bg_data/eco_regions/ecoNA_par.Rdata")
+ecoNA_sco <- readRDS(file = "./bg_data/eco_regions/ecoNA_sco.Rdata")
+ecoNA_sta <- readRDS(file = "./bg_data/eco_regions/ecoNA_sta.Rdata")
+ecoNA_ten <- readRDS(file = "./bg_data/eco_regions/ecoNA_ten.Rdata")
+ecoNA_uli <- readRDS(file = "./bg_data/eco_regions/ecoNA_uli.Rdata")
+ecoNA_vir <- readRDS(file = "./bg_data/eco_regions/ecoNA_vir.Rdata")
+ecoNA_vid <- readRDS(file = "./bg_data/eco_regions/ecoNA_vid.Rdata")
 
 
 # Crop wclim layers to ecoregions -----------------------------------------
-# V. angustifolium
 wclim_ang <- terra::crop(wclim, ecoNA_ang, mask = T)
 saveRDS(wclim_ang, file = './wclim_data/wclim_ang.Rdata')
 wclim_arb <- terra::crop(wclim, ecoNA_arb, mask = T)
 saveRDS(wclim_arb, file = './wclim_data/wclim_arb.Rdata')
-
+wclim_bor <- terra::crop(wclim, ecoNA_bor, mask = T)
+saveRDS(wclim_bor, file = './wclim_data/wclim_bor.Rdata')
+wclim_ces <- terra::crop(wclim, ecoNA_ces, mask = T)
+saveRDS(wclim_ces, file = './wclim_data/wclim_ces.Rdata')
+wclim_cor <- terra::crop(wclim, ecoNA_cor, mask = T)
+saveRDS(wclim_cor, file = './wclim_data/wclim_cor.Rdata')
+wclim_cra <- terra::crop(wclim, ecoNA_cra, mask = T)
+saveRDS(wclim_cra, file = './wclim_data/wclim_cra.Rdata')
+wclim_dar <- terra::crop(wclim, ecoNA_dar, mask = T)
+saveRDS(wclim_dar, file = './wclim_data/wclim_dar.Rdata')
+wclim_del <- terra::crop(wclim, ecoNA_del, mask = T)
+saveRDS(wclim_del, file = './wclim_data/wclim_del.Rdata')
+wclim_ery <- terra::crop(wclim, ecoNA_ery, mask = T)
+saveRDS(wclim_ery, file = './wclim_data/wclim_ery.Rdata')
+wclim_hir <- terra::crop(wclim, ecoNA_hir, mask = T)
+saveRDS(wclim_hir, file = './wclim_data/wclim_hir.Rdata')
+wclim_mac <- terra::crop(wclim, ecoNA_mac, mask = T)
+saveRDS(wclim_mac, file = './wclim_data/wclim_mac.Rdata')
+wclim_mem <- terra::crop(wclim, ecoNA_mem, mask = T)
+saveRDS(wclim_mem, file = './wclim_data/wclim_mem.Rdata')
+wclim_mys <- terra::crop(wclim, ecoNA_mys, mask = T)
+saveRDS(wclim_mys, file = './wclim_data/wclim_mys.Rdata')
+wclim_myr <- terra::crop(wclim, ecoNA_myr, mask = T)
+saveRDS(wclim_myr, file = './wclim_data/wclim_myr.Rdata')
+wclim_mtu <- terra::crop(wclim, ecoNA_mtu, mask = T)
+saveRDS(wclim_mtu, file = './wclim_data/wclim_mtu.Rdata')
+wclim_ova <- terra::crop(wclim, ecoNA_ova, mask = T)
+saveRDS(wclim_ova, file = './wclim_data/wclim_ova.Rdata')
+wclim_ovt <- terra::crop(wclim, ecoNA_ovt, mask = T)
+saveRDS(wclim_ovt, file = './wclim_data/wclim_ovt.Rdata')
+wclim_oxy <- terra::crop(wclim, ecoNA_oxy, mask = T)
+saveRDS(wclim_oxy, file = './wclim_data/wclim_oxy.Rdata')
+wclim_pal <- terra::crop(wclim, ecoNA_pal, mask = T)
+saveRDS(wclim_pal, file = './wclim_data/wclim_pal.Rdata')
+wclim_par <- terra::crop(wclim, ecoNA_par, mask = T)
+saveRDS(wclim_par, file = './wclim_data/wclim_par.Rdata')
+wclim_sco <- terra::crop(wclim, ecoNA_sco, mask = T)
+saveRDS(wclim_sco, file = './wclim_data/wclim_sco.Rdata')
+wclim_sta <- terra::crop(wclim, ecoNA_sta, mask = T)
+saveRDS(wclim_sta, file = './wclim_data/wclim_sta.Rdata')
+wclim_ten <- terra::crop(wclim, ecoNA_ten, mask = T)
+saveRDS(wclim_ten, file = './wclim_data/wclim_ten.Rdata')
+wclim_uli <- terra::crop(wclim, ecoNA_uli, mask = T)
+saveRDS(wclim_uli, file = './wclim_data/wclim_uli.Rdata')
+wclim_vir <- terra::crop(wclim, ecoNA_vir, mask = T)
+saveRDS(wclim_vir, file = './wclim_data/wclim_vir.Rdata')
+wclim_vid <- terra::crop(wclim, ecoNA_vid, mask = T)
+saveRDS(wclim_vid, file = './wclim_data/wclim_vid.Rdata')
 # Sample background points ------------------------------------------------
 set.seed(1337) # set a seed to ensure reproducible results
 
-# V. angustifolium
-ang_bg_vec <- spatSample(wclim_ang, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
-saveRDS(ang_bg_vec, file = './bg_data/ang_bg_vec.Rdata')
 # plot(wclim_ang[[1]])
 # points(ang_bg_vec, cex = 0.01)
+
+ang_bg_vec <- spatSample(wclim_ang, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(ang_bg_vec, file = './bg_data/ang_bg_vec.Rdata')
 arb_bg_vec <- spatSample(wclim_arb, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
 saveRDS(arb_bg_vec, file = './bg_data/arb_bg_vec.Rdata')
-plot(wclim_arb[[1]])
-points(arb_bg_vec, cex = 0.01)
+bor_bg_vec <- spatSample(wclim_bor, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(bor_bg_vec, file = './bg_data/bor_bg_vec.Rdata')
+ces_bg_vec <- spatSample(wclim_ces, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(ces_bg_vec, file = './bg_data/ces_bg_vec.Rdata')
+cor_bg_vec <- spatSample(wclim_cor, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(cor_bg_vec, file = './bg_data/cor_bg_vec.Rdata')
+cra_bg_vec <- spatSample(wclim_cra, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(cra_bg_vec, file = './bg_data/cra_bg_vec.Rdata')
+dar_bg_vec <- spatSample(wclim_dar, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(dar_bg_vec, file = './bg_data/dar_bg_vec.Rdata')
+del_bg_vec <- spatSample(wclim_del, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(del_bg_vec, file = './bg_data/del_bg_vec.Rdata')
+ery_bg_vec <- spatSample(wclim_ery, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(ery_bg_vec, file = './bg_data/ery_bg_vec.Rdata')
+hir_bg_vec <- spatSample(wclim_hir, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(hir_bg_vec, file = './bg_data/hir_bg_vec.Rdata')
+mac_bg_vec <- spatSample(wclim_mac, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(mac_bg_vec, file = './bg_data/mac_bg_vec.Rdata')
+mem_bg_vec <- spatSample(wclim_mem, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(mem_bg_vec, file = './bg_data/mem_bg_vec.Rdata')
+mys_bg_vec <- spatSample(wclim_mys, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(mys_bg_vec, file = './bg_data/mys_bg_vec.Rdata')
+myr_bg_vec <- spatSample(wclim_myr, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(myr_bg_vec, file = './bg_data/myr_bg_vec.Rdata')
+mtu_bg_vec <- spatSample(wclim_mtu, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(mtu_bg_vec, file = './bg_data/mtu_bg_vec.Rdata')
+ova_bg_vec <- spatSample(wclim_ova, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(ova_bg_vec, file = './bg_data/ova_bg_vec.Rdata')
+ovt_bg_vec <- spatSample(wclim_ovt, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(ovt_bg_vec, file = './bg_data/ovt_bg_vec.Rdata')
+oxy_bg_vec <- spatSample(wclim_oxy, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(oxy_bg_vec, file = './bg_data/oxy_bg_vec.Rdata')
+pal_bg_vec <- spatSample(wclim_pal, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(pal_bg_vec, file = './bg_data/pal_bg_vec.Rdata')
+par_bg_vec <- spatSample(wclim_par, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(par_bg_vec, file = './bg_data/par_bg_vec.Rdata')
+sco_bg_vec <- spatSample(wclim_sco, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(sco_bg_vec, file = './bg_data/sco_bg_vec.Rdata')
+sta_bg_vec <- spatSample(wclim_sta, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(sta_bg_vec, file = './bg_data/sta_bg_vec.Rdata')
+ten_bg_vec <- spatSample(wclim_ten, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(ten_bg_vec, file = './bg_data/ten_bg_vec.Rdata')
+uli_bg_vec <- spatSample(wclim_uli, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(uli_bg_vec, file = './bg_data/uli_bg_vec.Rdata')
+vir_bg_vec <- spatSample(wclim_vir, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(vir_bg_vec, file = './bg_data/vir_bg_vec.Rdata')
+vid_bg_vec <- spatSample(wclim_vid, 10000, 'random', na.rm = T, as.points = T) #ignore NA values
+saveRDS(vid_bg_vec, file = './bg_data/vid_bg_vec.Rdata')
